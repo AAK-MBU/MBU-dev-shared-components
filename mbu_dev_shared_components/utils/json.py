0450de724a2067a190720670cc107d1c59958aae
MBURKU <rku@aarhus.dk>
2024-05-17 21:52:57 +0200

    Changed logic ind add_key_value functionality.

--- mbu_dev_shared_components/utils/json.py
+++ mbu_dev_shared_components/utils/json.py
@@ -6,7 +6,7 @@ The primary class in this module is JSONManipulator, which contains methods for
 converting lists associated with keys in a JSON object into dictionaries. This
 is useful for restructuring JSON data into a more readable and accessible format.
 """
-from typing import Any, Dict, Union, List
+from typing import Any, Dict, List, Union
 
 
 class JSONManipulator:
@@ -15,55 +15,51 @@ class JSONManipulator:
 
     Methods
     -------
-    transform_all_lists(json_obj: Dict[str, Any], key_map: Dict[str, List[str]]) -> Dict[str, Any]:
+    transform_all_lists(json_obj: List[Union[List[Any], Dict[str, Any]]], key_map: List[str]) -> List[Union[Dict[str, Any], Any]]:
         Transforms all lists in the JSON object into dictionaries with specified keys.
 
-    add_key_value_to_node(node: Union[Dict[str, Any], Any], key_value_pairs: Dict[str, Any]) -> None:
+    add_key_value_to_node(node: Union[Dict[str, Any], List[Any]], key_value_pairs: Dict[str, Any]) -> None:
         Adds key-value pairs to a node in a JSON structure, supporting nested JSON structures.
     """
 
     @staticmethod
-    def transform_all_lists(json_obj: Dict[str, Any], key_map: Dict[str, List[str]]) -> Dict[str, Any]:
+    def transform_all_lists(json_obj: List[Union[List[Any], Dict[str, Any]]], key_map: List[str]) -> List[Union[Dict[str, Any], Any]]:
         """
         Transforms all lists in the JSON object into dictionaries with specified keys.
 
         Parameters
         ----------
-        json_obj : dict
+        json_obj : list
             The JSON object to be manipulated.
-        key_map : dict
-            A dictionary where each key is a JSON key and the value is a list of keys to be used for the new dictionaries.
+        key_map : list
+            The list of keys to be used for the new dictionaries.
 
         Returns
         -------
-        dict
+        list
             The updated JSON object with the transformed key-value pairs for all lists.
         """
-        if not isinstance(json_obj, dict):
-            raise TypeError("The input must be a dictionary.")
+        if not isinstance(json_obj, list):
+            raise TypeError("The input must be a list.")
 
-        def transform_lists(node: Any, key_map: Dict[str, List[str]]) -> Any:
-            if isinstance(node, list):
-                key = next((k for k, v in key_map.items() if len(v) == len(node)), None)
-                if key:
-                    return {key_map[key][j]: node[j] for j in range(len(key_map[key]))}
+        for i, item in enumerate(json_obj):
+            if isinstance(item, list):
+                if len(item) == len(key_map):
+                    json_obj[i] = {key_map[j]: item[j] for j in range(len(key_map))}
                 else:
-                    return node
-            elif isinstance(node, dict):
-                return {k: transform_lists(v, key_map) for k, v in node.items()}
-            else:
-                return node
-
-        return transform_lists(json_obj, key_map)
+                    raise ValueError(f"The length of the list at index {i} and the key_map must match.")
+            elif isinstance(item, dict):
+                JSONManipulator.transform_all_lists(list(item.values()), key_map)
+        return json_obj
 
     @staticmethod
-    def add_key_value_to_node(node: Union[Dict[str, Any], Any], key_value_pairs: Dict[str, Any]) -> None:
+    def add_key_value_to_node(node: Union[Dict[str, Any], List[Any]], key_value_pairs: Dict[str, Any]) -> None:
         """
         Adds key-value pairs to a node in a JSON structure, supporting nested JSON structures.
 
         Parameters
         ----------
-        node : Union[dict, any]
+        node : Union[dict, list]
             The JSON node to be manipulated.
         key_value_pairs : dict
             A dictionary of key-value pairs to be added to the node.
@@ -83,13 +79,13 @@ class JSONManipulator:
         }
         """
 
-        def add_key_values(target: Union[Dict[str, Any], Any], pairs: Dict[str, Any]) -> None:
+        def add_key_values(target: Union[Dict[str, Any], List[Any]], pairs: Dict[str, Any]) -> None:
             """
             Recursively adds key-value pairs to the target JSON node.
 
             Parameters
             ----------
-            target : Union[dict, any]
+            target : Union[dict, list]
                 The target JSON node to be manipulated.
             pairs : dict
                 A dictionary of key-value pairs to be added to the target.
@@ -102,7 +98,13 @@ class JSONManipulator:
                 else:
                     if isinstance(target, dict):
                         target[key] = value
+                    elif isinstance(target, list):
+                        for item in target:
+                            if isinstance(item, dict):
+                                item[key] = value
+                            else:
+                                raise TypeError(f"Cannot assign key-value pair to a non-dict item in list: {item}")
                     else:
-                        raise TypeError(f"Cannot assign key-value pair to non-dict target: {target}")
+                        raise TypeError(f"Cannot assign key-value pair to non-dict, non-list target: {target}")
 
         add_key_values(node, key_value_pairs)
