d215a409857c27561d5160449f345d8b395e88af
Sebastian <ahss@aarhus.dk>
2025-07-04 16:39:59 +0200

    Refactor database module

--- mbu_dev_shared_components/database/logging.py
+++ mbu_dev_shared_components/database/logging.py
@@ -2,85 +2,135 @@
 
 from datetime import datetime
 import time
-import os
 import socket
 
-from mbu_dev_shared_components.database.utility import execute_query, connect_to_db, fetch_env
-from mbu_dev_shared_components.utils.db_stored_procedure_executor import execute_stored_procedure
 
-def log_event(
-        log_db: str,
-        level: str, 
-        message: str, 
-        context: str, 
-        db_env="TEST"
+class Log:
+    """Base class for handling logging"""
+    def log_event(
+            self,
+            log_db: str,
+            level: str,
+            message: str,
+            context: str,
     ):
-    created_at = datetime.now()
-    """Logs the inputted parameters in """
-    query = f"""
-        INSERT INTO RPA.{log_db}
-            ([level]
-            ,[message]
-            ,[created_at]
-            ,[context])
-        VALUES
-            (?
-            ,?
-            ,?
-            ,?)
-        """
-    rpa_conn = connect_to_db(db_env=db_env)
-    cursor = rpa_conn.cursor()
+        """Logs the inputted parameters in """
+        created_at = datetime.now()
+        query = f"""
+            INSERT INTO RPA.{log_db}
+                ([level]
+                ,[message]
+                ,[created_at]
+                ,[context])
+            VALUES
+                (?
+                ,?
+                ,?
+                ,?)
+            """
 
-    params = [level, message, created_at, context]
-    execute_query(query=query, cursor=cursor, params=params)
+        params = [level, message, created_at, context]
+        self.execute_query(query=query, params=params)
 
-def _send_heartbeat(
-        servicename,
-        status,
-        details,
-        db_env,
-):
-    conn_env = fetch_env(db_env=db_env)
-    conn_str = os.getenv(conn_env)
-    hostname = socket.gethostname()
-    params = {
-        "ServiceName": (str, servicename), 
-        "Status": (str, status), 
-        "HostName": (str, hostname), 
-        "Details": (str, details)
-    }
-    result = execute_stored_procedure(
-        connection_string=conn_str,
-        stored_procedure='rpa.sp_UpdateHeartbeat',
-        params=params)
-    if result["success"] is not True:
-        print(result["error_message"])
-    
-def log_heartbeat(
-        stop: str|bool,
-        servicename: str,
-        heartbeat_interval: int,
-        details: str = "",
-        db_env: str = "PROD"
+    def _get_log_event(
+            self,
+            log_db: str,
+            level: str,
+            message: str,
+            context: str,
     ):
-    if isinstance(stop,str):
-        stop = stop == "True"
-    if not isinstance(heartbeat_interval, int):
-        heartbeat_interval = int(heartbeat_interval)
-    while not stop:
-        status = "RUNNING"
-        _send_heartbeat(
+        query = f"""
+            SELECT
+                ([level]
+                ,[message]
+                ,[created_at]
+                ,[context])
+            FROM
+                RPA.{log_db}
+            WHERE
+                level={level},
+                message={message},
+                context={context}
+            """
+
+        res = self.execute_query(query=query)
+        return res
+
+    def get_latest_log(
+            self,
+            log_db: str,
+    ):
+        query = f"""
+            SELECT TOP (1)
+                [level]
+                ,[message]
+                ,[created_at]
+                ,[context]
+            FROM
+                RPA.{log_db}
+            ORDER BY
+                created_at desc
+            """
+
+        res = self.execute_query(query=query)
+        return res
+
+    def _send_heartbeat(
+            self,
+            servicename,
+            status,
+            details
+    ):
+        """Function to send heartbeat to database"""
+        hostname = socket.gethostname()
+        params = {
+            "ServiceName": (str, servicename),
+            "Status": (str, status),
+            "HostName": (str, hostname),
+            "Details": (str, details)
+        }
+        result = self.execute_stored_procedure(
+            stored_procedure='rpa.sp_UpdateHeartbeat',
+            params=params)
+        if result["success"] is not True:
+            print(result["error_message"])
+
+    def log_heartbeat(
+            self,
+            stop: str | bool,
+            servicename: str,
+            heartbeat_interval: int,
+            details: str = "",
+    ):
+        """Function to log heartbeat"""
+        if isinstance(stop, str):
+            stop = stop == "True"
+        if not isinstance(heartbeat_interval, int):
+            heartbeat_interval = int(heartbeat_interval)
+        while not stop:
+            status = "RUNNING"
+            self._send_heartbeat(
+                servicename,
+                status,
+                details,
+            )
+            time.sleep(heartbeat_interval)
+        status = "STOPPED"
+        self._send_heartbeat(
             servicename,
             status,
             details,
-            db_env,
         )
-        time.sleep(heartbeat_interval)
-    status = "STOPPED"
-    _send_heartbeat(
-        servicename,
-        status,
-        details,
-        db_env,
-    )
+
+    def get_heartbeat(self, service_name: str):
+        """Get hearbeats """
+        query = f"""
+            SELECT
+                *
+            FROM
+                [RPA].[rpa].[ServiceHeartbeat]
+            WHERE
+                ServiceName = '{service_name}'
+        """
+        res = self.execute_query(query)
+        return res
